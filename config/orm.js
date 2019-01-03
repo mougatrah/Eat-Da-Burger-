9d635d4950920b3b459ee11ea876709cc9ce7bf7
PhelanM <phelanmccall@yahoo.com>
2019-01-03 00:44:49 -0800

    generic orm

--- config/orm.js
+++ config/orm.js
@@ -2,47 +2,92 @@ var connection = require("./connection.js");
 
 var orm = {
 
-    selectOne(name){
-        return connection.query(`SELECT * FROM burger WHERE burger_name = "${name}"`, function(err, res){
-            if(err) throw err;
-            console.table(res);
-        })
-    },
+    selectAll(table) {
 
-    selectAll(){
-        
-        return new Promise(function(resolve, reject){
-            connection.query(`SELECT * FROM burger`, function(err, res){
+        return new Promise(function (resolve, reject) {
+
+            connection.query(`SELECT * FROM ${table}`, function (err, res) {
                 if (err) throw err;
-                    if(!res.length){
-                        console.log("hello 1")
-                        reject(new Error("Error no length"));
-                    }else{
-                        for(let i in res){
-                            console.log(res[i]);
-                        }
-
-                        resolve(res);
+                if (!res.length) {
+                    console.log("hello 1")
+                    reject(new Error("Error no length"));
+                } else {
+                    for (let i in res) {
+                        console.log(res[i]);
                     }
-               
+                    resolve(res);
+                }
+
             })
+
         })
-      
-    },
-    insertOne(name){
-        
-        connection.query(`INSERT INTO burger (burger_name) VALUES ("${name}")`, function(err, res){
-            if(err) throw err;
-            console.log(res);
-        });
 
     },
-    updateOne(name , val){
-        connection.query(`UPDATE burger SET devoured = ${val} WHERE burger_name = "${name}"`, function(err, res){
-            if(err) throw err;
-            console.log(res);
+
+    selectOne(table, col, val) {
+
+        return Promise(function (resolve, reject) {
+
+            connection.query(`SELECT * FROM ${table} WHERE ${col} = "${val}"`, function (err, res) {
+
+                if (err) throw err;
+                if (!res.length) {
+                    console.log("hello 1")
+                    reject(new Error("Error no length"));
+                } else {
+                    for (let i in res) {
+                        console.log(res[i]);
+                    }
+                    resolve(res);
+                }
+            })
+
         })
+
+    },
+
+    insertOne(table, col, val) {
+
+        return new Promise(function (resolve, reject) {
+
+            connection.query(`INSERT INTO ${table} (${col}) VALUES ("${val}")`, function (err, res) {
+                if (err) throw err;
+                if (!res.length) {
+                    console.log("hello 1")
+                    reject(new Error("Error no length"));
+                } else {
+                    for (let i in res) {
+                        console.log(res[i]);
+                    }
+                    resolve(res);
+                }
+            })
+
+        })
+
+    },
+
+    updateOne(table, setCol, setVal, whereCol, whereVal) {
+
+        return new Promise(function (resolve, reject) {
+
+            connection.query(`UPDATE ${table} SET ${setCol} = ${setVal} WHERE ${whereCol} = "${whereVal}"`, function (err, res) {
+                if (err) throw err;
+                if (!res.length) {
+                    console.log("hello 1")
+                    reject(new Error("Error no length"));
+                } else {
+                    for (let i in res) {
+                        console.log(res[i]);
+                    }
+                    resolve(res);
+                }
+            })
+
+        })
+
     }
+
 }
 
 module.exports = orm;
